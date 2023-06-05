aff424d4e219fe0d6b898c1aa410984a8837a4e2
Ayyub Ibrahim RStudio <ayyubi-r@eleanor.hrdag.org>
2023-06-05 01:51:19 -0700

    applied trained model to entire index

--- classifier/apply/src/model.py
+++ classifier/apply/src/model.py
@@ -7,6 +7,10 @@ import argparse
 import logging
 import os
 from PIL import Image
+from PIL import UnidentifiedImageError
+
+Image.MAX_IMAGE_PIXELS = None
+
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -22,36 +26,35 @@ def change_dir(df):
     df.loc[:, "img_filepath"] = df.png.str.replace(r"^(.+)", r"../../thumbnail_accordian/\1", regex=True)
     df.loc[:, "img_filepath"] = df.img_filepath.fillna("").str.replace(r"\.\/nan", "", regex=True)
     df = df[~(df.img_filepath == "")]
-    df = df.sample(n=1000)
     return df
 
+from PIL import UnidentifiedImageError
 
-def apply_model(df, model):
-    dls = ImageDataLoaders.from_df(df, path="", fn_col='img_filepath', bs=64, normalize=True,
-                                   )
-    dl = dls.test_dl(df)  # Create a test dataloader for inference
-
-    # Use model.get_preds to get predictions for the test dataloader
+def apply_trained_model(df, model_path, device):
+    model = load_learner(model_path, cpu=False)
+    dls = ImageDataLoaders.from_df(df, path="", fn_col='img_filepath', bs=64, item_tfms=Resize((224, 224)))
+    dl = dls.test_dl(df)
+    dl = dl.to(device)  # Move the data to the correct device
     preds, _ = model.get_preds(dl=dl)
 
-    # Iterate over the predictions and assign scores to the dataframe
     for i, image_path in enumerate(df["img_filepath"]):
         try:
-            score = preds[i][1].item()  # Probability score for the positive class
+            score = preds[i][1].item()
             df.loc[df["img_filepath"] == image_path, "score"] = score
+        except UnidentifiedImageError as e:
+            logger.warning(f"Unidentified image error: {e}")
+            df.loc[df["img_filepath"] == image_path, "score"] = None
         except Exception as e:
             logger.warning(f"Error processing image {image_path}: {e}")
+            df.loc[df["img_filepath"] == image_path, "score"] = None
 
     return df
 
 
 if __name__ == '__main__':
     logger.info("Loading the model...")
-    model = load_learner(args.model)
-
-    logger.info("Setting up the device...")
+    model_path = args.model
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.model.to(device)
 
     logger.info("Loading the index")
     index = pd.read_csv(args.index)
@@ -59,13 +62,14 @@ if __name__ == '__main__':
 
     logger.info("Applying the model to each image...")
     try:
-        index = apply_model(index, model)
+        index = apply_trained_model(index, model_path, device)  # Pass the device as an argument
     except Exception as e:
         logger.error(f"An error occurred while applying the model: {e}")
 
     logger.info("Saving the updated index...")
     try:
-        index.to_csv(args.output, index=False)
+        output_path = args.output
+        index.to_csv(output_path, index=False)
     except Exception as e:
         logger.error(f"An error occurred while saving the updated index: {e}")
 
--- classifier/cnn/src/model.py
+++ classifier/cnn/src/model.py
@@ -7,10 +7,12 @@ from fastai.vision.all import *
 from fastai.vision.models import resnet34
 from fastai.vision.learner import *
 from fastai.vision.learner import cnn_learner
-from fastai.vision.augment import Rotate, RandomCrop, Flip
+from fastai.vision.augment import *
+from PIL import Image
 
+Image.MAX_IMAGE_PIXELS = None
 
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 parser = argparse.ArgumentParser()
@@ -24,7 +26,7 @@ def change_dir(df):
     df.loc[:, "img_filepath"] = df.png.str.replace(r"^(.+)", r"../../thumbnail_accordian/\1", regex=True)
     df.loc[:, "img_filepath"] = df.img_filepath.fillna("").str.replace(r"\.\/nan", "", regex=True)
     df = df[~(df.img_filepath == "")]
-    return df 
+    return df
 
 
 def assign_label_col(df):
@@ -32,22 +34,22 @@ def assign_label_col(df):
     df.loc[:, "label"] = 0
     df.loc[:, "doc_type"] = "unknown"
     df.loc[:, "label"] = df.label.astype(int)
-    return df 
+    return df
 
 
 def generate_reports(df):
-    df = df[(df.doc_type == "report")]
+    df = df[(df.doc_type == "transcript")]
     df.loc[:, "label"] = 1
     df.loc[:, "label"] = df.label.astype(int)
-    return df 
+    return df
 
 
 def concat(dfa, dfb):
     report_uids = [x for x in dfa["filehash"]]
     logger.info(f"Number of labeled reports: {len(dfa)}")
-                         
+
     dfb = dfb[~(dfb.filehash.isin(report_uids))]
-    dfb = dfb.sample(n=2500)
+    dfb = dfb.sample(n=28000)  # Sample a smaller number of non-labeled samples
     logger.info(f"Number of non-labeled samples: {len(dfb)}")
 
     df = pd.concat([dfa, dfb], axis=0)
@@ -55,74 +57,78 @@ def concat(dfa, dfb):
     df.loc[:, "label"] = df.label.astype(str).str.replace(r"(\.0)", "", regex=True).astype(int)
     logger.info(f"Number of samples in the final dataset: {len(df)}")
     logger.info(f"Class distribution:\n{df.label.value_counts(normalize=True)}")
-    return df 
+    return df
 
 
 def get_dataloaders(data):
-    train_aug = aug_transforms(mult=1.0, 
-                               do_flip=True, 
-                               flip_vert=False, 
-                               max_rotate=90.0, 
-                               min_zoom=1.0, 
-                               max_zoom=1.1, 
-                               max_lighting=0.2, 
-                               max_warp=0.2, 
-                               p_affine=0.75, 
-                               p_lighting=0.75, 
-                               xtra_tfms=[RandomCrop(size=(260, 260))])
-    
-    valid_aug = aug_transforms(mult=1.0, do_flip=False)
+    train_tfms = aug_transforms(
+        do_flip=True,
+        flip_vert=True,
+        max_rotate=15.0,
+        max_zoom=1.1,
+        max_lighting=0.2,
+        max_warp=0.2,
+        p_affine=0.75,
+        p_lighting=0.75,
+    )
+    valid_tfms = []
 
-    dls = ImageDataLoaders.from_df(data,
-            path="",
-            fn_col='img_filepath',
-            label_col='label',
-#             item_tfms=Resize((224,224)),
-#             batch_tfms=[*aug_transforms(size=(224, 224), pad_mode='zeros'), Flip(p=0.5)],
-            valid_pct=.3,
-            batch_size=32,
-            train_tfms=[],
-            valid_tfms=[]
-            )
+    dls = ImageDataLoaders.from_df(
+        data,
+        path="",
+        fn_col="img_filepath",
+        label_col="label",
+        item_tfms=Resize((224, 224)),  # Resize the images to (224, 224)
+        batch_size=16,
+        train_tfms=train_tfms,
+        valid_tfms=valid_tfms,
+    )
     return dls
 
 
 def setup_model(dls):
     gpu = torch.device("cuda")
-    learn = vision_learner(dls,
-            resnet34,
-            metrics=[error_rate, accuracy],
-            path=".",
-            model_dir=".")
+    learn = cnn_learner(
+        dls,
+        resnet34,
+        metrics=[error_rate, accuracy],
+        path=".",
+        model_dir=".",
+        pretrained=True,
+        ps=0.5,  # Dropout probability
+    )
     learn.to(gpu)
-    learn.dls.to(gpu)
+    learn.dls.device = gpu
     return learn
 
-if __name__ == '__main__':
-    tuning_n = 10
 
+def train_model(learn):
+    learn.fine_tune(10)
+    return learn
+
+
+if __name__ == "__main__":
+    # Load and preprocess labeled data
     df_lab = pd.read_csv(args.labeled)
     df_lab = change_dir(df_lab)
     reports = generate_reports(df_lab)
-    
+
+    # Load and preprocess non-labeled data
     df_nonlab = pd.read_csv(args.nonlabeled)
     df_nonlab = change_dir(df_nonlab)
     df_nonlab = assign_label_col(df_nonlab)
-    
+
+    # Concatenate the datasets
     train = concat(reports, df_nonlab)
-    
+
+    # Get data loaders
     dls = get_dataloaders(train)
+
+    # Setup the model
     learn = setup_model(dls)
-    
-    # Use lr_find to find optimal learning rate
-    lr = learn.lr_find()
-    logger.info(f"Optimal learning rate: {lr.valley}")
-    
-    # Train the model using the optimal learning rate
-    learn.fine_tune(tuning_n, lr.valley)
-    
+
+    # Train the model
+    learn = train_model(learn)
+
+    # Save the trained model
     learn.export(args.output)
-    
-    # Output accuracy score based on validation data
-    acc = learn.validate()[2]
-    logger.info(f"Accuracy: {acc}")
